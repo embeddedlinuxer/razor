f9a20707aa123ad9cc5d026e2806fc93b29ba3a4
Daniel <dkoh@phasedynamics.com>
2020-10-13 08:49:32 -0500

    removed REG_DIAGNOSTICS

--- Calculate.c
+++ Calculate.c
@@ -89,9 +89,6 @@ void Poll(void)
 	Uint8 err_w = FALSE;	// watercut calculation error
 	Uint8 err_d = FALSE;	// density correction error
 
-    /// Read DIAGNOSTICS
-    REG_DIAGNOSTICS = DIAGNOSTICS;
-
     /// Read RTC
     Read_RTC(&CAL_RTC_SEC, &CAL_RTC_MIN, &CAL_RTC_HR, &CAL_RTC_DAY, &CAL_RTC_MON, &CAL_RTC_YR);
 
